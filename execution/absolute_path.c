4ac7fb84e0fef6630470424b390b9fd3180ca145
Youness Ezzemryouy <yezzemry@c2r5p9.1337.ma>
2025-04-23 14:16:35 +0100

    y

--- execution/absolute_path.c
+++ execution/absolute_path.c
@@ -14,5 +14,29 @@
 
 int	main()
 {
-	
+	t_list	*head = NULL;
+	t_list	*node1 = malloc(sizeof(t_list));
+	t_list	*node2 = malloc(sizeof(t_list));
+	t_list	*node3 = malloc(sizeof(t_list));
+	if (!node1 || !node2 || !node3)
+		return (1);
+	head = node1;
+	node1->content = "ls";
+	node1->prev = NULL;
+	node1->type = ;
+	node1->next = node2;
+
+	node2->content = "-a";
+	node2->prev = node1;
+	node2->next = node3;
+
+	node3->content = "|";
+	node3->prev = node2;
+	node3->next = node4;
 }
+// ./a.out "ls  -a | cat -e"
+// ls
+// -a
+// |
+// cat
+// -e

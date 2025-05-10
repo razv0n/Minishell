96be9a23f4f5fef261843445d679171c54e4919a
Mouncif Fahmi <mfahmi@c2r5p12.1337.ma>
2025-05-10 10:35:28 +0100

    git pull

--- allocate_free/init_data.c
+++ allocate_free/init_data.c
@@ -6,7 +6,7 @@
 /*   By: mfahmi <mfahmi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 15:32:59 by mfahmi            #+#    #+#             */
-/*   Updated: 2025/05/08 23:57:12 by mfahmi           ###   ########.fr       */
+/*   Updated: 2025/05/10 10:33:21 by mfahmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,24 @@ void    init_info(t_info *info)
     // info->head_env = NULL;
     info->head_cmd = NULL;
     info->content = NULL;
+}
+
+void    get_head(void *ptr)
+{
+    static t_ptr *head;
+    ft_lstadd_back_d(&head, ft_lstnew_d(ptr));
+}
+void   *ft_malloc (size_t size)
+{
+    void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+	{
+		perror(ERR_MALLOC);
+		// ft_free();// should free all
+		exit(1);
+	}
+    get_head(ptr);
+	return (ptr);
 }
